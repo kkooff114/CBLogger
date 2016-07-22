8390d53be1ae3f80a7e0d1c1e168b4d5520b82b0
Lou Jiwei <jiwei.lou@foxmail.com>
2016-07-22 14:28:14 +0800

    new function

--- app/src/main/java/android/loujiwei/cn/cbloggersample/MainActivity.java
+++ app/src/main/java/android/loujiwei/cn/cbloggersample/MainActivity.java
@@ -2,19 +2,43 @@ package android.loujiwei.cn.cbloggersample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import cn.loujiwei.cblogger.logger.CBLogger;
+import cn.loujiwei.cblogger.utils.FileUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView totalSize, deleteAll, deleteDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        totalSize = (TextView) findViewById(R.id.tv_total_size);
+        deleteAll = (TextView) findViewById(R.id.tv_delet_all);
+        deleteDay = (TextView) findViewById(R.id.tv_delet_7);
 
-        for (int i = 0; i < 100; i++) {
-            CBLogger.d(""+i);
+        for (int i = 0; i < 20; i++) {
+            CBLogger.d("" + i);
         }
+
+
+        totalSize.setText("总大小" + CBLogger.getLogFolderSize() + "/ 空余:" + FileUtils.getSDAvailableSize() / 1024 / 1024);
+        deleteAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CBLogger.deleteLogFolder();
+            }
+        });
+
+        deleteDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CBLogger.deleteLogFolderByDay(1);
+            }
+        });
     }
 }
--- cblogger2/src/main/java/cn/loujiwei/cblogger/logger/CBLogger.java
+++ cblogger2/src/main/java/cn/loujiwei/cblogger/logger/CBLogger.java
@@ -2,6 +2,7 @@ package cn.loujiwei.cblogger.logger;
 
 import cn.loujiwei.cblogger.utils.FileUtils;
 import cn.loujiwei.cblogger.utils.ThreadManager;
+
 import android.os.Environment;
 
 import java.text.SimpleDateFormat;
@@ -18,27 +19,48 @@ public class CBLogger {
     public final static String fileName = format.format(new Date()) + ".log";
 
     private static int maxLength = 10;
+    private static int deleteDay = 7;   //自动删除几天前的log
     static StringBuffer _stringBuffer = new StringBuffer(maxLength);
 
-    private static String logPaht = Environment.getExternalStorageDirectory()
+    private static String logPath = Environment.getExternalStorageDirectory()
             + "/cestbon/baseData/log/";
 
 
     public static void d(String d) {
-        System.out.println(":"+d);
+        System.out.println(":" + d);
         _stringBuffer.append(d);
 
-        System.out.println("stringBuffer = "+_stringBuffer.toString());
+        System.out.println("stringBuffer = " + _stringBuffer.toString());
 
         testWriteBufferToDisk();
+    }
+
+    /**
+     * 设置最大缓存大小
+     *
+     * @param length
+     */
+    public static void setMaxLength(int length) {
+        CBLogger.maxLength = length;
+        CBLogger._stringBuffer = new StringBuffer(length);
+    }
+
+
+    /**
+     * 设置log的存放路径
+     *
+     * @param path
+     */
+    public static void setLogPath(String path) {
+        logPath = path;
     }
 
 
     public static synchronized void testWriteBufferToDisk() {
 
-        System.out.println("长度----->"+_stringBuffer.length());
+        System.out.println("长度----->" + _stringBuffer.length());
         if (_stringBuffer.length() > maxLength) {
-            System.out.println("超了 "+_stringBuffer.length());
+            System.out.println("超了 " + _stringBuffer.length());
             final String cache = _stringBuffer.toString();
 
             _stringBuffer.delete(0, _stringBuffer.length());
@@ -47,10 +69,46 @@ public class CBLogger {
                 @Override
                 public String call() throws Exception {
 
-                    return FileUtils.fileWrite(logPaht, fileName, cache);
+                    if (FileUtils.getSDAvailableSize() < 10) {
+                        FileUtils.deleteLogFolderBeforeDays(logPath, deleteDay);
+                    }
+                    return FileUtils.fileWrite(logPath, fileName, cache);
 
                 }
             });
         }
+    }
+
+
+    /**
+     * 获得log文件夹的大小
+     *
+     * @return
+     */
+    public static String getLogFolderSize() {
+        return FileUtils.getAutoFileOrFilesSize(logPath);
+    }
+
+    /**
+     * 删除log文件夹
+     *
+     * @return
+     */
+    public static boolean deleteLogFolder() {
+        return FileUtils.deleteDirectory(logPath);
+    }
+
+    /**
+     * 删除day天前的照片
+     *
+     * @param day
+     * @return
+     */
+    public static boolean deleteLogFolderByDay(int day) {
+        return FileUtils.deleteLogFolderBeforeDays(logPath, day);
+    }
+
+    public static void setDeleteDay(int deleteDay) {
+        CBLogger.deleteDay = deleteDay;
     }
 }
